3926d86be81475ca928a6ca2952358af7b7e87f0
Michael FIG <mfig@agoric.com>
2020-03-02 19:21:25 -0600

    fix: upgrade from react-gallery3

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,8 +1,14 @@
-import React, { useState, useEffect, useLayoutEffect, useCallback, useRef } from 'react';
-import useStayScrolled from 'react-stay-scrolled';
+import React, {
+  useState,
+  useEffect,
+  useMemo,
+  useRef,
+} from 'react';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
 
 import './App.css';
 
+/*
 // Let's pretend this <Counter> component is expensive to re-render so ...
 // ... we wrap with React.memo, but we're still seeing performance issues :/
 // So we add useWhyDidYouUpdate and check our console to see what's going on.
@@ -45,12 +51,14 @@ function useWhyDidYouUpdate(name, props) {
     previousProps.current = props;
   });
 }
+*/
 
-/* global WebSocket fetch */
+/* global fetch */
 const RECONNECT_BACKOFF_SECONDS = 3;
 
 async function call(req) {
   // console.log(req);
+  return {state: "[[]]"}; // TODO
   const res = await fetch('/vat', {
     method: 'POST',
     body: JSON.stringify(req),
@@ -63,183 +71,50 @@ async function call(req) {
   throw new Error(`server error: ${JSON.stringify(j.rej)}`);
 }
 
-function useWebSocket(wsURL, onMessage, onOpen) {
-  const [isOnline, setIsOnline] = useState(null);
 
-  useEffect(
-    function run() {
-      const ws = new WebSocket(wsURL);
-      ws.addEventListener('open', _ev => {
-        setIsOnline(true);
-        console.log(`ws.open!`);
-        onOpen(onMessage).catch(_ => ws.close());
-      });
-      ws.addEventListener('close', _ev => {
-        setIsOnline(false);
-        console.log(`Reconnecting in ${RECONNECT_BACKOFF_SECONDS} seconds`);
-        setTimeout(run, RECONNECT_BACKOFF_SECONDS * 1000);
-      });
-      ws.addEventListener('message', ev => {
-        try {
-          // console.log('ws.message:', ev.data);
-          const obj = JSON.parse(ev.data);
-          onMessage(obj);
-        } catch (e) {
-          console.log(`error handling message`, e);
-        }
-      });
-
-      return function cleanup() {
-        return ws.close();
-      };
-    },
-    [wsURL, onMessage, onOpen],
-  );
-  return { isOnline };
-}
-
-const Pixel = React.memo(function ({k: k, color}) {
-  useWhyDidYouUpdate('Pixel', { k, color });
-  console.log(`${k}`);
+const Pixel = React.memo(function Pixel({ color, generation }) {
+  // useWhyDidYouUpdate('Pixel', { color, generation });
   return (
-      <div key={`${k}`} style={{ backgroundColor: color }} />
-    );
+    <div
+      className={`updated${generation % 2}`}
+      style={{ backgroundColor: color }}
+    />
+  );
 });
 
 function Gallery({ board }) {
-  let i = 0;
+  let pxs = [];
+  // We need to render increasing x followed by increasing y.
+  const maxHeight = board.reduce((prior, column) => Math.max(prior, column.length), 0);
+  for (let y = 0; y < maxHeight; y += 1) {
+    for (let x = 0; x < board.length; x += 1) {
+      const [color, generation] = board[x][y];
+      pxs.push(<Pixel
+        key={`${x},${y}`}
+        color={color}
+        generation={generation}
+      />);
+    }
+  }
   return (
-    <div id="galleryBoard" className="gallery">
-      {board.flatMap(row =>
-        row.map(color => <Pixel k={i++} color={color} />),
-      )}
-    </div>
+    <div id="galleryBoard" className="gallery">{pxs}</div>
   );
 }
 
-function Repl({ history, connected, sendCommand }) {
-  const bgColor = connected ? 'white' : 'red';
-  const [commandEntry, setCommandEntry] = useState('');
-  const [prevEntry, setPrevEntry] = useState('');
-  const [entryI, setEnryI] = useState(null);
-
-  const historyRef = useRef(null);
-  // const { stayScrolled } = useStayScrolled(historyRef);
-  // useLayoutEffect(() => {
-  //   stayScrolled();
-  // }, [history, stayScrolled]);
-
-  function onSubmit(event) {
-    event.preventDefault();
-    sendCommand(commandEntry);
-    setEnryI(null);
-    setCommandEntry('');
-  }
-
-  // remember what the user changed the comment to
-  function onChange(e) {
-    const newCommand = e.target.value;
-    setCommandEntry(newCommand);
-    setPrevEntry(newCommand);
-    // console.log(`MEMO ${newCommand}`);
-  }
-
-  function moveHistory(delta) {
-    // history from 30 by 1 for 31
-    // console.log(`history from ${entryI} by ${delta} for ${history.length}`);
-    // null means we are working on a new entry rather than from a copied history entry
-    const newI = (entryI === null ? history.length : entryI) + delta;
-    if (newI < 0 || newI > history.length) {
-      // Do nothing.
-      return;
-    }
-    if (newI === history.length) {
-      // console.log(`history to null for ${newI}`);
-      setEnryI(null);
-      setCommandEntry(prevEntry);
-    } else {
-      // console.log(`history to ${newI}`);
-      setEnryI(newI);
-      setCommandEntry(history[newI].command);
+function calculateBoard(colors, oldBoard = []) {
+  const newBoard = [];
+  for (let x = 0; x < colors.length; x += 1) {
+    newBoard.push([]);
+    for (let y = 0; y < colors[x].length; y += 1) {
+      const [oldColor, oldGen] =
+        (oldBoard && oldBoard[x] && oldBoard[x][y]) || [];
+      const newColor = colors[x][y];
+      const gen = oldGen || 0;
+      const newGen = oldColor === newColor ? gen : gen + 1;
+      newBoard[x].push([newColor, newGen]);
     }
   }
-
-  function onKeyup(e) {
-    // console.log(`key ${e.key}`);
-    switch (e.key) {
-      case 'ArrowUp':
-        moveHistory(-1);
-        break;
-
-      case 'ArrowDown':
-        moveHistory(+1);
-        break;
-      case 'p':
-        if (e.ctrlKey) {
-          moveHistory(-1);
-        }
-        break;
-
-      case 'n':
-        if (e.ctrlKey) {
-          moveHistory(+1);
-        }
-        break;
-
-      // Do the standard behaviour.
-      default:
-        // skip the preventDefaults below
-        return;
-    }
-    // for used events.
-    e.preventDefault();
-  }
-
-  const historyElements = [];
-  history.forEach(({ number, command, result }) => {
-    historyElements.push(
-      <div key={`c${number}`} className="command-line">
-        <div>command[{number}]</div>
-        <div>{command}</div>
-      </div>,
-      <div key={`h${number}`} className="history-line">
-        <div>history[{number}]</div>
-        <div>{result}</div>
-      </div>,
-    );
-  });
-
-  return (
-    <React.Fragment>
-      <div className="help">
-        Use <code>home</code> to see useful objects, and <code>history[N]</code>{' '}
-        to refer to result history
-      </div>
-      <div id="history" ref={historyRef} className="history">
-        {historyElements}
-      </div>
-      <div className="history">
-        <form id="command-entry" className="command-line" onSubmit={onSubmit}>
-          <div>command[{history.length}]</div>
-          <div>
-            <input
-              id="input"
-              tabIndex="0"
-              type="text"
-              style={{ backgroundColor: bgColor }}
-              value={commandEntry}
-              onChange={onChange}
-              onKeyUp={onKeyup}
-              name="command entry"
-            />
-          </div>
-          <div>
-            <input id="go" tabIndex="1" type="submit" value="eval" name="eval" />
-          </div>
-        </form>
-      </div>
-    </React.Fragment>
-  );
+  return newBoard;
 }
 
 // history updates (promises being resolved) and canvas updates
@@ -248,6 +123,7 @@ function Repl({ history, connected, sendCommand }) {
 function App({ wsURL }) {
   const [history, setHistory] = useState([]);
   const [board, setBoard] = useState([]);
+  const didUnmount = useRef(false);
 
   const sendCommand = command => {
     console.log('submitEval', command);
@@ -256,7 +132,25 @@ function App({ wsURL }) {
     call({ type: 'doEval', number, body: command }).catch(ex => console.log(ex));
   };
 
-  const onMessage = useCallback(obj => {
+  const options = useMemo(() => ({
+    onOpen: () => call({ type: 'getCanvasState' })
+      .then(obj => setBoard(oldBoard => calculateBoard(JSON.parse(obj.state), oldBoard)))
+      .catch(e => console.error(e))
+      .then(_ => call({ type: 'rebroadcastHistory' })),
+    shouldReconnect: closeEvent => {
+      return didUnmount.current === false;
+    },
+    reconnectAttempts: 10,
+    reconnectInterval: RECONNECT_BACKOFF_SECONDS * 1000,
+  }), []);
+
+  const [sendMessage, lastMessage, readyState] = useWebSocket(wsURL, options);
+
+  useEffect(() => {
+    if (lastMessage === null) return;
+
+    const obj = JSON.parse(lastMessage);
+
     // we receive commands to update result boxes
     if (obj.type === 'updateHistory') {
       // these args come from calls to vat-http.js updateHistorySlot()
@@ -273,41 +167,18 @@ function App({ wsURL }) {
       });
     } else if (obj.type === 'updateCanvas') {
       // console.log(obj);
-      setBoard(JSON.parse(obj.state));
+      setBoard(oldBoard => calculateBoard(JSON.parse(obj.state), oldBoard));
     } else {
       console.log(`unknown WS type in:`, obj);
     }
-  }, []);
+  }, [lastMessage]);
 
-  const onOpen = useCallback(() =>
-    call({ type: 'getCanvasState' })
-      .then(obj => setBoard(JSON.parse(obj.state)))
-      .then(_ => call({ type: 'rebroadcastHistory' })),
-    []);
-
-  const { isOnline } = useWebSocket(wsURL, onMessage, onOpen);
+  useEffect(() => () => didUnmount.current = true);
 
   return (
     <div>
-      <div className="container">
-        <div className="left">
-          <Gallery board={board} />
-        </div>
-        <div className="right">
-          <Repl
-            history={history}
-            connected={isOnline}
-            sendCommand={sendCommand}
-          />
-        </div>
-      </div>
-
-      <hr />
-      <address>
-        Source: <a target="_blank" id="package_repo">
-          <span id="package_name">cosmic-swingset</span></a> v
-        <span id="package_version"></span>+<span id="package_git"></span>
-      </address>
+      <div>{readyState === ReadyState.OPEN ? 'Online' : 'Offline'}</div>
+      <Gallery board={board} />
     </div>
   );
 }
