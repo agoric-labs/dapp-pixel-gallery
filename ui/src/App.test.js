e10fec7f83253ca3254baef09c867b0201c41c13
Michael FIG <mfig@agoric.com>
2020-03-03 09:49:35 -0600

    test: pass basic rendering test

--- ui/src/App.test.js
+++ ui/src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import WS from 'jest-websocket-mock';
+
 import App from './App';
 
 it('renders without crashing', () => {
+  const wsURL = 'ws://pixel-gallery.testnet.example.com:8000';
+  const server = new WS(wsURL);
+  server.connected.then(_ => server.close());
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App wsURL={wsURL} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
